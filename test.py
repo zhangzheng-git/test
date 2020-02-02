f970e81a0828462e36de610f73a02f538ef1afea
zhang <398488036@qq.com>
2020-02-02 16:20:01 +0800

    说明

--- test.py
+++ test.py
@@ -1,25 +1,33 @@
+# 不换行
 # print("你好",end="python")
 # print("are you ok")
+#类型显示转换
 # print(int("4")+4)
+# 类型函数type()
 # print(type("6"))
-
+# 列表
 arry = ["zhagn",1,4.3,True,[1,2,2]]
+# 长度函数
 print(len(arry))
-
+#列表函数，插入
 # arry.insert(2,99)
 # arry.append(3)
+#列表函数，删除
 # del arry[1]
 # arry.pop(0)
 # print(arry)
+#反向打印
 # print(arry[0:-2])
+#字符串实质 字符序列
 # str = "zhanghzrng"
 # print(str)
-list_str  = ["duhei","wnv","wqon","qui","af"]
+#排序 正向
+#list_str  = ["duhei","wnv","wqon","qui","af"]
 # list_str.sort()
-
-print(sorted(list_str))
-print(list_str)
-
+#排序 反向
+# print(sorted(list_str))
+# print(list_str)
+#集合是无序的
 number = (78,90)
 print(number)
 a = set("abddsd")
@@ -30,15 +38,7 @@ print(a|b)
 list2 = ["32","i",29,392]
 print("i" in list2)
 print(78 in number)
-
-
-age = 909
-if age == 99:
-    print("A")
-print("B")
-
-
-
+#if elif
 num = 0
 if num > 1:
     print(1)
@@ -48,15 +48,15 @@ elif num > 3:
     print(3)
 else:
     print(5)
+
 num  = 0
 sum =-1
 while num<100:
-
     sum += num
     num +=1
 print(sum)
 
-for num in range(101):
+for num in range(10):
     print(num)
 
 list9=["a","b","e","r","t"]
@@ -81,3 +81,11 @@ def add(a,b):
     return (a+b)
 
 print(add(9,8))
+
+def pr(a,b,c):
+    for i in range(0,a):
+        for j in range(i):
+            print(c,end="")
+        print()
+
+pr(7,7,"*")
