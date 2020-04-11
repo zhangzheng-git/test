2179c34574ff90d46fd591e93288a6714ff53bbf
zhangzheng <398488036@qq.com>
2020-04-11 09:38:37 +0800

    ab

--- varible/main.py
+++ varible/main.py
@@ -20,8 +20,8 @@ from pyecharts.faker import Faker
 c = (
     Bar()
     .add_xaxis(Faker.choose())
-    .add_yaxis("商家A", Faker.values())
-    .add_yaxis("商家B", Faker.values())
+    .add_yaxis("简介A", Faker.values())
+    .add_yaxis("简介B", Faker.values())
     .set_global_opts(
         title_opts=opts.TitleOpts(title="Bar-显示 ToolBox"),
         toolbox_opts=opts.ToolboxOpts(),
@@ -35,3 +35,6 @@ def sub(a,b,c):
 
 def sub(a,b,c):
     return a+b-c
+
+def order(x,y):
+    return x*y
